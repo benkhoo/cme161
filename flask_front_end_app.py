b4e9d9b0d9c117628f13abda883f3abd8873ccd2
Ben Khoo <benkhoo@DN51vtfc.SUNet>
2016-05-31 15:41:15 -0700

    testing pushing data

--- flask_front_end_app.py
+++ flask_front_end_app.py
@@ -28,25 +28,25 @@ def get_trellis_limit(n_entries):
 
 def make_data_graph(data_list_in):
 	idx = 0
-		names = collections.OrderedDict()
-		for e in data_list_in:
-			to = e['to'][:7] # truncate
-			fr = e['from'][:7]
-			if to not in names:
-				names[to] = idx
-				idx += 1
-			if fr not in names:
-				names[fr] = idx
-				idx += 1
-		edges = [{
-					"source": names[e['to'][:7]], 
-					"target": names[e['from'][:7]], 
-					"value": e['n'], 
-					"tags":  [d['tag'] for d in e['data']] 
-				} for e in data_list_in
-			]
-		nodes = [{"name":n} for n in names.keys()]
-		return { "nodes": nodes, "edges": edges }
+	names = collections.OrderedDict()
+	for e in data_list_in:
+		to = e['to'][:7] # truncate
+		fr = e['from'][:7]
+		if to not in names:
+			names[to] = idx
+			idx += 1
+		if fr not in names:
+			names[fr] = idx
+			idx += 1
+	edges = [{
+				"source": names[e['to'][:7]], 
+				"target": names[e['from'][:7]], 
+				"value": e['n'], 
+				"tags":  [d['tag'] for d in e['data']] 
+			} for e in data_list_in
+		]
+	nodes = [{"name":n} for n in names.keys()]
+	return { "nodes": nodes, "edges": edges }		
 
 @app.route('/graph', methods=['GET'])
 def get_graph():
